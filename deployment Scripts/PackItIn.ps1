cf4220f2160d8908aebbb9c06797f2fe9a0cc4af
GilesWakefieldEduserv <giles.wakefield@eduserv.org.uk>
2016-08-05 14:46:00 +0100

    piles of rubbish.  probably doomed.  cspack works though.

---
+++ deployment Scripts/PackItIn.ps1
@@ -0,0 +1,18 @@
+﻿# path to cspack
+$cspackPath = Join-Path $env:ProgramFiles 'Microsoft SDKs\Azure\.NET SDK\v2.9\bin\cspack.exe'
+
+$PackagePath = '..\lab6.cspkg'
+$serviceDefinitionFile = '..\Lab6.AzureCloudService\ServiceDefinition.csdef'
+$webRoleName = 'WebRole1'
+$webRolePath = '..\'
+
+# define the cspack parameters
+$cspackParameter = @(
+        "/out:$PackagePath",
+        $serviceDefinitionFile,
+        "/role:$webRoleName;$webRolePath",
+        "/sites:$webRoleName;Web;$webRolePath"
+    )
+
+# execute cspack
+& $cspackPath @cspackParameter
